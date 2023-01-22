9573e0807399eac17932d208bb1193845c455aa1
Alex Zaslavskis <sahsariga111@gmail.com>
2023-01-22 14:16:04 +0000

    Update build.py

--- contacts_to_birthdays/build.py
+++ contacts_to_birthdays/build.py
@@ -1,12 +1,16 @@
 import os
 import datetime
 
-
 def version():
+    """
+    Prints the current version of the project.
+    """
     print("v0.0.1 alpha by alex5250")
-pass
-
+    pass
 
 def build_docs():
-    os.system('pdoc  -d markdown ./contacts_to_birthdays/main.py  -o ./docs --logo "https://placedog.net/300?random"')
-pass
+    """
+    Builds the documentation for the project using pdoc.
+    """
+    os.system('pdoc -d markdown ./contacts_to_birthdays/main.py  -o ./docs --logo "https://placedog.net/300?random"')
+    pass
