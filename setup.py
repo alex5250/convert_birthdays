bc79c49a771838fbc61788e15544c85fca320113
Alex Zaslavskis <sahsariga111@gmail.com>
2023-10-03 19:47:29 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='contacts_to_birthdays',
+    version='0.1.0',
+    author='Alex Zaslavskis',
+    author_email='sahsariga111@gmail.com',
+    description='Awesome .vcf/instagram birthday converter.',
+    packages=find_packages(),
+    install_requires=[
+        'datetime',
+        'pandas',
+        'xlwt',
+        'beautifulsoup4',
+        'python-dateutil',
+        'ics'
+    ],
+    entry_points={
+        'console_scripts': [
+            'contacts_to_birthdays = __main__:main'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
